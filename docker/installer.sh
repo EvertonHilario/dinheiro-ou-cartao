6eb4b32d97a3cab899bc07e59993e921d754c010
evertonhilario <evertonjuru@gmail.com>
2021-04-21 14:12:30 -0300

    style: comments

--- docker/installer.sh
+++ docker/installer.sh
@@ -10,30 +10,40 @@
 
 echo "INSTALANDO APP"
 echo ""
-echo "                                              0%"
+echo "                                                    0%"
 echo ""
 
 echo "1 - Subindo os containers"
 echo ""
+
 docker-compose up -d
 echo ""
-echo "========                                     25%"
+echo "==========                                          20%"
 echo ""
 
 echo "2 - Instalando dependências via composer"
 echo ""
 docker exec dinheiro-ou-cartao composer install
 echo ""
-echo "===================                          50%"
+echo "====================                                40%"
 echo ""
 
 echo "3 - criando estrutura para salvar os dados"
 docker exec dinheiro-ou-cartao php artisan migrate
 echo ""
-echo "====================================         75%"
+echo "==============================>                     60%"
 echo ""
 
-echo "4 - Salvando dados base (Tipos de usuários, 3 usuários, tipo de transações, status de transações, tipo de operações)"
+echo "4 - Salvando dados base (Tipos de usuários, 3 usuários, tipo de transações, status de transações, tipo de operações e deposita 700.00 pila na carteira do Éverton Hilario)"
 docker exec dinheiro-ou-cartao php artisan db:seed --class=estrutura_apresentacao
 echo ""
-echo "===========================================> 100%"
+echo "========================================>           80%"
+
+echo ""
+
+echo "5 - Instanciando a execução dos Jobs (maximo de 5 tentativas para consumir uma mensagem)"
+echo ""
+
+echo "==================================================> 100%"
+echo "Monitorando os jobs..."
+docker exec dinheiro-ou-cartao php artisan queue:work
