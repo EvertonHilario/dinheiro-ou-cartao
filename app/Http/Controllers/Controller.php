71a00f32a1cc994fc5442431ce94ab4598ebc966
evertonhilario <evertonjuru@gmail.com>
2021-04-23 11:20:48 -0300

    feat: added responseAdapter method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,12 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-
+    public function responseAdapter($code = 200, $message, $data = [])
+    {
+        return response()->json([
+            'code' => $code,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
 }
