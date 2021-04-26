a05a49a96ad38c4c41f0d7d80726d98872dcfd78
evertonhilario <evertonjuru@gmail.com>
2021-04-26 05:16:14 -0300

    feat: installer.sh

--- docker/installer.sh
+++ docker/installer.sh
@@ -11,39 +11,50 @@
 echo "INSTALANDO APP"
 echo ""
 echo "                                                    0%"
-echo ""
 
+echo ""
 echo "1 - Subindo os containers"
 echo ""
-
 docker-compose up -d
 echo ""
-echo "==========                                          20%"
-echo ""
+echo "=======>                                            15%"
 
+echo ""
 echo "2 - Instalando dependências via composer"
 echo ""
 docker exec dinheiro-ou-cartao composer install
 echo ""
-echo "====================                                40%"
-echo ""
+echo "==============>                                     30%"
 
+echo ""
 echo "3 - criando estrutura para salvar os dados"
 docker exec dinheiro-ou-cartao php artisan migrate
 echo ""
-echo "==============================>                     60%"
-echo ""
+echo "========================>                           45%"
 
+echo ""
 echo "4 - Salvando dados base (Tipos de usuários, 3 usuários, tipo de transações, status de transações, tipo de operações e deposita 700.00 pila na carteira do Éverton Hilario)"
 docker exec dinheiro-ou-cartao php artisan db:seed --class=estrutura_apresentacao
 echo ""
-echo "========================================>           80%"
+echo "================================>                   60%"
 
 echo ""
-
-echo "5 - Instanciando a execução dos Jobs (maximo de 5 tentativas para consumir uma mensagem)"
+echo "5 - Rodando testes unitários"
 echo ""
+docker exec dinheiro-ou-cartao vendor/bin/phpunit tests/Unit/
+echo ""
+echo "======================================>             75%"
 
+echo ""
+echo "5 - Rodando testes de integração"
+echo ""
+docker exec dinheiro-ou-cartao vendor/bin/phpunit tests/Integration/
+echo ""
+echo "============================================>       90%"
+
+echo ""
+echo "6 - Instanciando a execução dos Jobs"
+echo ""
 echo "==================================================> 100%"
 echo "Monitorando os jobs..."
-docker exec dinheiro-ou-cartao php artisan queue:work
+docker exec dinheiro-ou-cartao php artisan queue:listen
